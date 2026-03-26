c104866ebe12b1c8e149a3bd9ee3bd1b6ee2e78a
Yared <yared@gmail.com>
2026-03-26 05:57:19 +0300

    we fix it

--- my-netflix/src/App.js
+++ my-netflix/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Banner from './banner';
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="app">
-      {/* Nav bar goes here */}
-      
-      <Banner />
-      
-      {/* Movie rows will be added here */}
-      {/* <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} /> */}
-      {/* <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> */}
+    <Home/>
     </div>
   );
 }
