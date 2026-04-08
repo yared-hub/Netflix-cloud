4ef15bde2e719d560e30b40d1e987051f8f97252
Yared <yared@gmail.com>
2026-04-08 05:11:39 +0300

    sdv

--- my-netflix/src/banner.js
+++ my-netflix/src/banner.js
@@ -3,17 +3,24 @@ import './banner.css';
 
 function banner() {
   return (
-    <header className="banner">
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: url("https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"),
+        backgroundPosition: "center center",
+      }}
+    >
       <div className="banner_contents">
         <h1 className="banner_title">The Movie Title</h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
+        <h1 className="banner_description">
+          When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests...
+        </h1>
       </div>
-      <h1 className='banner_description'>"When the menace known as the Joker wreaks
-         havoc and chaos on the people of Gotham, Batman must accept one of the greatest 
-         psychological and physical tests of his ability to fight injustice."</h1>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
