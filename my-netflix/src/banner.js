86454d468bc22f4392e0189b4053d6b9fab72815
Yared <yared@gmail.com>
2026-04-08 05:36:31 +0300

    zxbm.

--- my-netflix/src/banner.js
+++ my-netflix/src/banner.js
@@ -12,15 +12,12 @@ function Banner() {
     >
       <div className="banner_contents">
         <h1 className="banner_title">The Movie Title</h1>
-        
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-
         <h1 className="banner_description">
-          When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, 
-          Batman must accept one of the greatest psychological and physical tests.
+          When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests...
         </h1>
       </div>
       <div className="banner--fadeBottom" />
