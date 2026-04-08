38435747918a2c6d25aefdb166c1d363fbae3af3
Yared <yared@gmail.com>
2026-04-08 04:45:45 +0300

    buthe

--- my-netflix/src/pages/home/home.js
+++ my-netflix/src/pages/home/home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import './banner.css';
 
-const Home = () => {
+function banner() {
     return (
-        <div>Home</div>
-    )
+        <header className="banner">
+            <div className="banner_contents">
+                <h1 className="banner_title">The Movie Title</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">
+                    When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.
+                </h1>
+            </div>
+        </header>
+    );
 }
 
-export default Home
+export default banner;
