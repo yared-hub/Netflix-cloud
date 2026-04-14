9caef6503a892367ccd166da959e976c8ea6c346
Yared <yared@gmail.com>
2026-04-14 12:01:36 +0300

    cb

--- my-netflix/src/requests.js
+++ my-netflix/src/requests.js
@@ -3,7 +3,6 @@ const API_KEY = "ee3807d091fc7a183c81234d0da21406";
 const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  // ሌሎችንም መጨመር ትችላለህ...
 };
 
 export default requests;
