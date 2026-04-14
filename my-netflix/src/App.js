34ef46a81adca2b710893cc9bb98c28903d155c2
Yared <yared@gmail.com>
2026-04-14 12:41:25 +0300

    bvc

--- my-netflix/src/App.js
+++ my-netflix/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home/home';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
-    <div className="app">
-    <Home/>
+    <div className="App">
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      {/* ሌሎችንም እንደዚሁ መቀጠል ትችላለህ */}
     </div>
   );
 }
