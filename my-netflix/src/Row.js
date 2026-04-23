c0c900dab9fb3ada1a686830d705b470f84e7490
Yared <yared@gmail.com>
2026-04-23 05:31:42 +0300

    ምን

--- my-netflix/src/Row.js
+++ my-netflix/src/Row.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'; 
-function Row({ title, fetchUrl }) {
-   const [movies, setMovies] = useState([]);
+import './Row.css'; 
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
@@ -14,13 +18,13 @@ function Row({ title, fetchUrl }) {
 
     return (
         <div className="row">
-            <h2>{title}</h2>
+            <h2 className="row__title">{title}</h2>
             <div className="row__posters">
                 {movies?.map(movie => (
                     <img 
                         key={movie.id}
-                        className="row__poster"
-                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} 
                     />
                 ))}
