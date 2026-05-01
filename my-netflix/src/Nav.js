9f1b581b9846acec1d1d1fd75a96d3c1ab61aede
Yared <yared@gmail.com>
2026-05-01 08:27:52 +0300

    ችቭ

--- my-netflix/src/Nav.js
+++ my-netflix/src/Nav.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import './Nav.css';
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
 
-function Nav() {
+function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   return (
-    <div className="nav">
-      <img 
-        className="nav_logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" 
-        alt="Netflix Logo" 
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
       />
-      <img 
-        className="nav_avatar"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
-        alt="Netflix Avatar" 
-      />
+      <div className="nav__links">
+        <span>Home</span>
+        <span>TV Shows</span>
+        <span>Movies</span>
+        <span>New & Popular</span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Nav;
