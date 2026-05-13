04fd3e9ff016ba07cbf03aa79c8807c2ba04b7f5
Yared <yared@gmail.com>
2026-05-13 14:12:20 +0300

    fix it

---
+++ my-netflix/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHaPZBY5cHDj-4yFLe23u60JEkY-uMNjI",
+  authDomain: "netf-4810c.firebaseapp.com",
+  projectId: "netf-4810c",
+  storageBucket: "netf-4810c.firebasestorage.app",
+  messagingSenderId: "316552035864",
+  appId: "1:316552035864:web:b6308cfbb0f14914010d6d",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
