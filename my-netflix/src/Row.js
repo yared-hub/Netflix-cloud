efbfdf37027bed3b9ff0f7043e10c65fe65c713a
Yared <yared@gmail.com>
2026-05-13 16:17:11 +0300

    vb

--- my-netflix/src/Row.js
+++ my-netflix/src/Row.js
@@ -1,23 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from './axios';
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
 import './Row.css';
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
 
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow, search }) {
+
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
+
     async function fetchData() {
+
       const request = await axios.get(fetchUrl);
+
       setMovies(request.data.results);
+
       return request;
     }
+
     fetchData();
+
   }, [fetchUrl]);
 
-  // ለYouTube ማሳያ የሚሆኑ ሴቲንግስ
+  const handleClick = (movie) => {
+
+    if (trailerUrl) {
+
+      setTrailerUrl("");
+
+    } else {
+
+      movieTrailer(movie?.title  movie?.name  "")
+        .then((url) => {
+
+          const urlParams = new URLSearchParams(new URL(url).search);
+
+          setTrailerUrl(urlParams.get("v"));
+
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie?.title?.toLowerCase().includes(search.toLowerCase()) ||
+    movie?.name?.toLowerCase().includes(search.toLowerCase())
+  );
+
   const opts = {
     height: "390",
     width: "100%",
@@ -26,37 +57,35 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
-  // ምስሉ ሲነካ የሚሰራ ፈንክሽን
-  const handleClick = (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl(""); // ቀድሞ የተከፈተ ካለ ይዘጋል
-    } else {
-      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
-        .then((url) => {
-          // የዩቲዩብ ሊንኩን ወደ ID ለመቀየር
-          const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get("v"));
-        })
-        .catch((error) => console.log(error));
-    }
-  };
-
   return (
     <div className="row">
+
       <h2>{title}</h2>
-      <div className="row__posters">
-        {movies.map((movie) => (
+
+      <div className="row_posters">
+
+        {filteredMovies.map((movie) => (
+
           <img
             key={movie.id}
-            onClick={() => handleClick(movie)} // እዚህ ጋር ክሊክ ሲደረግ ይጠራል
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+
+            onClick={() => handleClick(movie)}
+
+            className={row_poster ${isLargeRow && "row_posterLarge"}}
+
+            src={https://image.tmdb.org/t/p/original/${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }}
+
             alt={movie.name}
           />
+
         ))}
+
       </div>
-      {/* ትሬይለር URL ካለ ቪዲዮውን ያሳያል */}
+
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+
     </div>
   );
 }
