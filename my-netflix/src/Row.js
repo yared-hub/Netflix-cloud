bbd7b36420f61ef6fb37d73eb0995393f56bb1e6
Yared <yared@gmail.com>
2026-05-23 06:37:13 +0300

    fgbn

--- my-netflix/src/Row.js
+++ my-netflix/src/Row.js
@@ -70,11 +70,11 @@ const filteredMovies = movies.filter((movie) => {
       <h2>{title}</h2>
 
       <div className="row__posters">
-  {filteredMovies.filter((movie) =>
-      movie.poster_path || movie.backdrop_path
-  )
-    .map((movie) => (
+ {(search ? filteredMovies : movies)
+  .filter((movie) => movie.poster_path || movie.backdrop_path)
+  .map((movie) => (
 
+    
   <div key={movie.id}>
 
     <img
